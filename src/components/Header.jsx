4dd2b2a919088e646ee28bb91db27826458d5a5a
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-02-12 01:48:33 +0200

    start project with grid-area

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ96lrvoXbEiXlbj9_Czn6FwsFICjdoxiN_KQ&usqp=CAU" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () =>{
+    return(
+        <div className='content'>
+        <div>
+        <img src="https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/t9ur9cc1khkup1dmcbzd/%D0%9F%D0%B0%D1%80%D0%BA%D1%80%D0%B0%D0%B7%D0%B2%D0%BB%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B9IMGWorldsofAdventure.webp" alt=""/>
+        </div>
+        <div>
+          ava + des
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    )
+}
+
+export default Profile

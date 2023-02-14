e59dad0a2fa40bb7d99d51ae6a575f18128bc297
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-02-14 15:31:12 +0200

    attempts to use props in the project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,9 +6,9 @@ const MyPosts = () => {
     return(
         <div className={style.wrapper}>
             <h2>Some think about me</h2>
-          <Post/>
-          <Post/>
-          <Post/>
+          <Post header='Patrik' content='I`m happy cat' counter='2' date='12/03/02'/>
+          <Post header='Sirco' content='I`m fell good and you?' counter='19' date='11/03/02'/>
+          <Post header='Misa' content='Me first post' counter='2333' date='12/03/01'/>
         </div>
     )
 }
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import style from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={style.container}>
             <div className={style.img}>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM5pL9EknuomjF1shh2CYTGcwpWzh41ZzwUg-AOak&s" alt="cat"/>
             </div>
             <div className={style.content}>
-                <h3>Post header</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cum voluptatibus nisi corrupti delectus autem quos exercitationem labore eius quisquam.</p>
-                <span>23/04/05</span>
+                <h3>{props.header}</h3>
+                <p>{props.content}</p>
+                <span>{props.date}</span>
                 
             </div>
-            <button>like</button>
+            <button>Like {props.counter}</button>
 
            
         </div>
